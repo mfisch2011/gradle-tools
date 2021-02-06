9e7ff79cd95ce98d3f2f1c7a6e70e409c148a3e1
M. Fischer <mfisch2011@gmail.com>
2021-02-06 17:11:28 -0500

    ...

--- plugins/install-distribution/src/main/java/com/github/mfisch2011/gradle/tasks/InstallDistribution.java
+++ plugins/install-distribution/src/main/java/com/github/mfisch2011/gradle/tasks/InstallDistribution.java
@@ -81,8 +81,12 @@ public class InstallDistribution extends DefaultTask {
 				filename = filename.substring(index+1);
 				Path filePath = dest.resolve(filename);
 	            if (!entry.isDirectory()) {
-	            	getLogger().info("Copying {} to {}.",entry.getName(),filePath);
-	                Files.copy(stream,filePath,StandardCopyOption.REPLACE_EXISTING);
+	            	if(filePath.toFile().exists()) {
+	            		getLogger().info("Skipping {}.",filePath);
+	            	} else {
+		            	getLogger().info("Copying {} to {}.",entry.getName(),filePath);
+		                Files.copy(stream,filePath,StandardCopyOption.REPLACE_EXISTING);
+	            	}
 	            } else {
 	                // if the entry is a directory, make the directory
 	                File dir = filePath.toFile();
